3092ff7bf5f3edbe8fefdb7a5af17745ad21f5e7
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2020-04-13 09:00:43 +0300

    Updated license
    The license has been changed, in order to make OnionSproutsBot with source code taken directly from the gettor project.

--- src/bot.py
+++ src/bot.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2020
-# Panagiotis Vasilopoulos <hello@alwayslivid.com>
+# This file is part of OnionSpoutsBot, a Tor Browser distribution Telegram bot.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# :authors: Panagiotis Vasilopoulos <hello@alwayslivid.com>
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
+# :copyright:   (c) 2020, Panagiotis Vasilopoulos
 #
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
+# :license: This is Free Software. See LICENSE for license information.
+#
 
 import telebot
 from telebot import types
